05901df46186ce1d380d9a1d6680b2f16f2348cb
pratikshad16 <dhagepratiksha16@gmail.com>
2020-09-20 13:36:59 +0530

    Added: Ability to create a person information

---
+++ Address_Book/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Address_Book
+{
+    class Person
+    {
+        private String firstName;
+        private String lastName;
+        private String address;
+        private String city;
+        private String state;
+        private String phoneNumber;
+        private String zipCode;
+        public Person(string firstName, string lastName, string address, string city, string state, string phoneNumber, string zipCode)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.phoneNumber = phoneNumber;
+            this.zipCode = zipCode;
+        }
+
+
+        public string LastName { get => lastName; set => lastName = value; }
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string Adderss { get => address; set => address = value; }
+        public string City { get => city; set => city = value; }
+        public string State { get => state; set => state = value; }
+        public string PnoneNumber { get => phoneNumber; set => phoneNumber = value; }
+        public string ZipCode { get => zipCode; set => zipCode = value; }
+
+
+        public override string ToString()
+        {
+            return "[firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", city=" + city
+                 + ", state=" + state + ", zipCode=" + zipCode + ", PhoneNumber=" + phoneNumber + "]" + "\n";
+        }
+    }
+}
+
